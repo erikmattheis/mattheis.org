0ab54e86aaf111a08a4cf7bf16bfce07fd8fe0df
Erik Mattheis <erik@mattheis.org>
2023-12-08 02:12:23 -0600

    add analytics

--- app/main.js
+++ app/main.js
@@ -1,47 +1,86 @@
 import './style.css'
 
-
-// after everything is loaded
-
-  function randomPercentage() {
-    return (Math.random() * 200 - 100) + '%';
-    }
-window.onload = () => {
-
-  var stylesheet = document.styleSheets[0];
-
-for (var i = 0; i < stylesheet.cssRules.length; i++) {
-  var rule = stylesheet.cssRules[i];
-
-  if (rule.type === CSSRule.KEYFRAMES_RULE && rule.name === "move") {
-    // This is the "move" keyframes rule
-    console.log(rule);
-  }
+function randomPercentage() {
+  let result;
+  do {
+    result = Math.random() * 3 - 1.5;
+  } while (result > -1 && result < 1);
+  return result;
 }
 
-  var stylesheet = document.styleSheets[0];
+function changeCSSAnimationRotationRandomlyAndSlightly() {
+  const swingElements = document.querySelectorAll('.swing');
+  const stylesheet = document.styleSheets[0];
 
-  const ruleList = document.styleSheets[0].cssRules;
+  swingElements.forEach((element, index) => {
+    const animationName = 'swing' + index;
+    const rotation = randomPercentage();
+    const duration = Math.abs(rotation) * 1.5;
 
-  console.log('lll', JSON.stringify(ruleList));
+    const keyframes = `
+      @keyframes ${animationName} {
+        0% { transform: rotate(-${rotation}deg); }
+        50% { transform: rotate(${rotation}deg); }
+        100% { transform: rotate(-${rotation}deg); }
+      }
+    `;
 
-  
+    stylesheet.insertRule(keyframes, stylesheet.cssRules.length);
 
-  stylesheet.deleteRule(0);
-  
-  var keyframes = `
-  @keyframes move {
-    0% { 
-      background-position: ${randomPercentage()}% ${randomPercentage()}%, ${randomPercentage()}% ${randomPercentage()}%, ${randomPercentage()}% ${randomPercentage()}%; 
-    }
-    50% {
-      background-position: ${randomPercentage()}% ${randomPercentage()}%, ${randomPercentage()}% ${randomPercentage()}%, ${randomPercentage()}% ${randomPercentage()}%; 
-    }
-    100% {
-      background-position: ${randomPercentage()}% ${randomPercentage()}%, ${randomPercentage()}% ${randomPercentage()}%, ${randomPercentage()}% ${randomPercentage()}%; 
-    }
-  }
-`;
-stylesheet.insertRule(keyframes, 0);
+    element.style.animation = `${animationName} ${duration}s infinite ease-in-out`;
+    element.style.transformOrigin = "center -200px";
+  });
 }
 
+function addListeners() {
+  const swingElements = document.querySelectorAll('.swing');
+
+  swingElements.forEach(element => {
+    element.addEventListener('mouseover', function () {
+      element.style.animationPlayState = 'paused';
+    });
+  });
+
+  swingElements.forEach(element => {
+    element.addEventListener('mouseout', function () {
+      element.style.animationPlayState = 'running';
+    });
+  });
+
+  swingElements.forEach(element => {
+    element.addEventListener('touchstart', function () {
+      element.style.animationPlayState = 'paused';
+    });
+  });
+
+  swingElements.forEach(element => {
+    element.addEventListener('touchend', function () {
+      element.style.animationPlayState = 'running';
+    });
+  });
+
+  const svgs = document.getElementsByClassName('svg');
+  console.log(svgs)
+  svgs.forEach(element => {
+    element.addEventListener('mouseover', function () {
+      document.getElementById('message').style.display = 'block';
+    });
+  });
+
+  svgs.forEach(element => {
+    console.log('OGVIYTVYVGI')
+
+    element.addEventListener('touchstart', function () {
+      document.getElementById('message').style.display = 'block';
+    });
+  });
+}
+
+
+window.onload = function () {
+
+  console.log('window.onload')
+  addListeners();
+  changeCSSAnimationRotationRandomlyAndSlightly();
+
+}
