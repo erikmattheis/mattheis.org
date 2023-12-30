de72b079b08f16e0421addacea11ac094b332ed8
Erik Mattheis <erik@mattheis.org>
2023-12-30 02:04:57 -0600

    srtar

--- app/main.js
+++ app/main.js
@@ -1,5 +1,94 @@
-import './style.css'
+const welcomeSplash = `<div class="page">
+<div class="card">
+<div class="loader-container">
+  <div class="modal-loader">
+    <h1 class="loader animated dur06 blurred">Let's Build Something Great Together</h1>
+    <div class="svg animated dur06 blurred">
+      <svg class="ov-icon ov-wrench" viewBox="0.48 0.48 23.04 23.04">
+        <path fill="#ffffff"
+          d="M20.97 7.27a.996.996 0 000-1.41l-2.83-2.83a.996.996 0 00-1.41 0l-4.49 4.49-3.89-3.89c-.78-.78-2.05-.78-2.83 0l-1.9 1.9c-.78.78-.78 2.05 0 2.83l3.89 3.89L3 16.76V21h4.24l4.52-4.52 3.89 3.89c.95.95 2.23.6 2.83 0l1.9-1.9c.78-.78.78-2.05 0-2.83l-3.89-3.89 4.48-4.48zM5.04 6.94l1.89-1.9L8.2 6.31 7.02 7.5l1.41 1.41 1.19-1.19 1.2 1.2-1.9 1.9-3.88-3.88zm11.23 7.44l-1.19 1.19 1.41 1.41 1.19-1.19 1.27 1.27-1.9 1.9-3.89-3.89 1.9-1.9 1.21 1.21zM6.41 19H5v-1.41l9.61-9.61 1.3 1.3.11.11L6.41 19zm9.61-12.44l1.41-1.41 1.41 1.41-1.41 1.41-1.41-1.41z">
+        </path>
+      </svg>
+    </div>
+  </div>
+  <
+</div>
+</div>`
 
+document.body.insertAdjacentHTML('beforeend', welcomeSplash);
+
+let loaderTimeout;
+
+function removeLoading() {
+  const loader = document.getElementsByClassName('loader-container');
+  if (loader[0]) {
+  loader[0].remove();
+}
+  
+}
+
+function hideLoading() {
+
+  clearTimeout(loaderTimeout);
+
+  const loader = document.getElementsByClassName('loader-container');
+
+  if (loader[0]){
+    loader[0].classList.add('transparent');
+  }
+
+  document.removeEventListener('mousemove', hideLoading);
+  document.removeEventListener('touchstart', hideLoading);
+  animateContent();
+  setTimeout(removeLoading, 1000);
+}
+
+function animateLoader() {
+  const allElements = document.getElementsByClassName('loader');
+  for(let i = 0; i < allElements.length; i++) {
+    allElements[i].classList.remove('left', 'right', 'blurred');
+  }
+}
+
+//animateLoader();
+
+function animateContent() {
+  const allElements = document.getElementsByTagName('*');
+  for(let i = 0; i < allElements.length; i++) {
+    allElements[i].classList.remove('left', 'right', 'blurred');
+  }
+}
+
+window.onload = function() {
+
+  loaderTimeout = setTimeout(hideLoading, 500);
+
+  document.addEventListener('mousemove', hideLoading);
+  document.addEventListener('touchstart', hideLoading);
+  addAnimations();
+}
+
+/*
+function addAnimations() {
+let elements = document.querySelectorAll('.topic');
+
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        console.log('intersecting')
+        entry.target.classList.remove('left', 'right', 'blurred');
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+
+  elements.forEach(element => {
+    observer.observe(element);
+  });
+}
+*/
+
+/*
 function randomPercentage() {
   let result;
   do {
@@ -110,3 +199,5 @@ window.onload = function () {
   //changeCSSAnimationRotationRandomlyAndSlightly();
 
 }
+
+*/
