6fa054a09c4c67cd939a210fb8f63b49d6883a99
Erik Mattheis <erik@mattheis.org>
2024-01-08 19:55:24 -0600

    Simplify backgrounds

--- app/main.js
+++ app/main.js
@@ -8,13 +8,11 @@ Saint Paul, MN
 </footer>`;
 
 function addActiveButton(subjectId) {
-
   const button = document.getElementById(`button-${subjectId}`);
 
   if (button) {
     button.classList.add("active");
   }
-
 }
 
 function removeActiveButton(subjectId) {
@@ -24,11 +22,9 @@ function removeActiveButton(subjectId) {
   if (currentActiveButton) {
     currentActiveButton.classList.remove("active");
   }
-
 }
 
 function removeActiveSubject(subjectId) {
-
   const currentActiveSubject = document.querySelector(".subject-active");
 
   if (currentActiveSubject) {
@@ -38,7 +34,6 @@ function removeActiveSubject(subjectId) {
 }
 
 function attachTransitionEndListener(subjectId) {
-
   const subject = document.getElementById(subjectId);
 
   if (subject) {
@@ -61,7 +56,6 @@ function attachTransitionEndListeners() {
 }
 
 function addActiveSubject(subjectId) {
-
   const newActiveSubject = document.getElementById(subjectId);
 
   if (newActiveSubject) {
@@ -97,7 +91,6 @@ function handleClick(event, subjectId) {
   addActiveButton(subjectId);
   removeActiveSubject(subjectId);
   addActiveSubject(subjectId);
-
 }
 
 handleClick(dummyEvent, 'approach');
@@ -115,7 +108,6 @@ function adjustSubjectWrapperHeight() {
 }
 
 function attachListeners() {
-
   const buttonAi = document.getElementById("button-work");
   const buttonJs = document.getElementById("button-tools");
   const buttonApproach = document.getElementById("button-approach");
@@ -152,32 +144,7 @@ function removeLoading() {
   }
 }
 
-function hideLoading() {
-
-
-  const loader = document.getElementsByClassName("loader-container");
-
-  if (loader[0]) {
-    loader[0].classList.add("transparent");
-  }
-
-  document.removeEventListener("mousemove", hideLoading);
-  document.removeEventListener("touchstart", hideLoading);
-
-  animateContent();
-
-  setTimeout(removeLoading, 1000);
-}
-
-function animateLoader() {
-  const allElements = document.getElementsByClassName("loader");
-  for (let i = 0; i < allElements.length; i++) {
-    allElements[i].classList.remove("left", "right", "blurred");
-  }
-}
-
 function animateContent() {
-
   const allElements = document.getElementsByClassName("header");
   for (let i = 0; i < allElements.length; i++) {
     allElements[i].classList.remove("left", "right", "blurred");
@@ -206,114 +173,3 @@ window.onload = function () {
   attachListeners();
   animateContent();
 };
-
-
-/*
-window.addEventListener("scroll", function () {
-  return;
-  const topics = document.querySelectorAll(".topic");
-
-  const noElement = {
-    getPropertyValue: function () {
-      return 0;
-    },
-  };
-
-  const contentPadding = 16;
-
-  const animationHeight = window.innerHeight * 0.2;
-
-  topics.forEach(function (topic) {
-    const rect = topic.getBoundingClientRect();
-
-    const distanceFromTop = rect.bottom - contentPadding;
-    const distanceFromBottom = window.innerHeight - rect.top;
-
-    if (distanceFromTop > 0 && distanceFromTop < animationHeight) {
-      const translateX = distanceFromTop - animationHeight;
-
-      topic.style.transform = "translateX(" + translateX + "px)";
-      topic.style.opacity = distanceFromTop / animationHeight;
-    } else if (distanceFromBottom > 0 && distanceFromBottom < animationHeight) {
-      const translateX = animationHeight - distanceFromBottom;
-
-      topic.style.transform = "translateX(" + translateX + "px)";
-      topic.style.opacity = distanceFromBottom / animationHeight;
-    }
-  });
-
-  const copies = document.querySelectorAll(".copy");
-
-  copies.forEach(function (copy) {
-    let copyStyle;
-
-    if (copy) {
-      copyStyle = window.getComputedStyle(copy);
-    } else {
-      copyStyle = noElement;
-    }
-
-    const rect2 = copy.getBoundingClientRect();
-
-    const distanceFromTop2 = rect2.bottom - contentPadding;
-    const distanceFromBottom2 = window.innerHeight - rect2.top;
-
-    if (distanceFromTop2 > 0 && distanceFromTop2 < animationHeight) {
-      const translateX = distanceFromTop2 - animationHeight;
-
-      copy.style.transform = "translateX(" + translateX + "px)";
-      copy.style.opacity = distanceFromTop2 / animationHeight;
-    } else if (
-      distanceFromBottom2 > 0 &&
-      distanceFromBottom2 < animationHeight
-    ) {
-      const translateX = animationHeight - distanceFromBottom2;
-
-      copy.style.transform = "translateX(" + translateX + "px)";
-      copy.style.opacity = distanceFromBottom2 / animationHeight;
-      return;
-    }
-  });
-});
-
-
-function removeLoading() {
-  const loader = document.getElementsByClassName("loader-container");
-  if (loader[0]) {
-    loader[0].remove();
-  }
-}
-
-function hideLoading() {
-
-
-  const loader = document.getElementsByClassName("loader-container");
-
-  if (loader[0]) {
-    loader[0].classList.add("transparent");
-  }
-
-  document.removeEventListener("mousemove", hideLoading);
-  document.removeEventListener("touchstart", hideLoading);
-
-  animateContent();
-
-  setTimeout(removeLoading, 1000);
-}
-
-function animateLoader() {
-  const allElements = document.getElementsByClassName("loader");
-  for (let i = 0; i < allElements.length; i++) {
-    allElements[i].classList.remove("left", "right", "blurred");
-  }
-}
-
-function animateContent() {
-
-  const allElements = document.getElementsByClassName("header");
-  for (let i = 0; i < allElements.length; i++) {
-    allElements[i].classList.remove("left", "right", "blurred");
-  }
-}
-
-*/
